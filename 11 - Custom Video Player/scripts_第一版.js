e308a055c822d0174c94c797495078691c95d7ca
kanboo <wang.kanboo@gmail.com>
2018-03-05 00:01:38 +0800

    更新

--- 11 - Custom Video Player/scripts_第一版.js
+++ 11 - Custom Video Player/scripts_第一版.js
@@ -1,28 +1,36 @@
 /* 1.取得所有video DOM元素 */
+
 const video = document.querySelector('.viewer');
 const toggleBtn = document.querySelector('.toggle');
 const progress = document.querySelector('.progress');
 const progressBar = document.querySelector('.progress__filled');
-const ranges = document.querySelectorAll('.player__slider');
+const volume = document.querySelector('[name="volume"]');
+const playbackRate = document.querySelector('[name="playbackRate"]');
 const skipButtons = document.querySelectorAll('[data-skip]');
+
 
 
 
 /* 2.相關function */
 function togglePlay(e){
-    const method = video.paused ? 'play' : 'pause';
-    video[method]();
+    if (video.paused === true){
+        toggleBtn.textContent = "❚ ❚";
+        video.play();
+    }else{
+        toggleBtn.textContent = "►";
+        video.pause();
+    } 
 }
 
-function updateButton() {
-    const icon = this.paused ? '►' : '❚ ❚';
-    // console.log(icon);
-    toggleBtn.textContent = icon;
+
+function handleVolume(e){
+    // console.log(this.value);
+    video.volume = this.value
 }
 
-
-function handleRangeUpdate() {
-    video[this.name] = this.value;
+function handlePlaybackRate(e){
+    // console.log(this.value);
+    video.playbackRate = this.value
 }
 
 function handleProgress(e){
@@ -48,41 +56,25 @@ function skip() {
     video.currentTime += parseFloat(this.dataset.skip);
 }
 
-
-
 /* 3.監聽DOM元素 */
 // video 播放或暫停
 video.addEventListener('click',togglePlay);
 toggleBtn.addEventListener('click',togglePlay);
 
-// 監聽是否切換播放的icon
-video.addEventListener('play', updateButton);
-video.addEventListener('pause', updateButton);
-
 // 進度條更新目前播放時間
-// video.addEventListener('timeupdate', handleProgress);
-video.addEventListener('progress', handleProgress);
+video.addEventListener('timeupdate', handleProgress);
 
 // 進度條監聽是否有切換video播放時間
 let mousedown = false; //判斷滑鼠是否有點擊
 progress.addEventListener('click', scrub);
-
-/* 此段程式碼，可簡寫為下面程式碼
-progress.addEventListener('mousemove', (e) => {
-    if (mousedown) {
-        scrub(e);
-    }
-});
-*/
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
-
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
 
-// 調整 聲音、播放倍數
-ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
-ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
-
+// 調整聲音
+volume.addEventListener('change', handleVolume);
+// 調整播放倍數
+playbackRate.addEventListener('change', handlePlaybackRate);
 
 // 快進、快退(+5 or -5)
 skipButtons.forEach(button => button.addEventListener('click', skip));
