1576b4e24a2a10c2f38e21f42c80411251459df4
kanboo <wang.kanboo@gmail.com>
2018-05-27 20:58:26 +0800

    updata

---
+++ script.js
@@ -0,0 +1,283 @@
+var datas = [{
+    no: 1,
+    title: 'JavaScript Drum Kit',
+    desc: '製作一個可透過按鍵發聲的爵士鼓',
+    demo: 'https://kanboo.github.io/JavaScript30/01%20-%20JavaScript%20Drum%20Kit/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/01%20-%20JavaScript%20Drum%20Kit/index.html',
+    blog: 'https://kanboo.github.io/2018/02/13/JS30-01-JavaScript-Drum-Kit/',
+  },
+  {
+    no: 2,
+    title: 'JS and CSS Clock',
+    desc: '利用 JS 與 CSS 搭配作出時鐘效果',
+    demo: 'https://kanboo.github.io/JavaScript30/02%20-%20JS%20and%20CSS%20Clock/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/02%20-%20JS%20and%20CSS%20Clock/index.html',
+    blog: 'https://kanboo.github.io/2018/02/14/JS30-02-JS-and-CSS-Clock/',
+  },
+  {
+    no: 3,
+    title: 'Playing with CSS Variables and JS',
+    desc: '利用 CSS變數 與 JS 即時更新圖片的 內距、邊框色、模糊 效果',
+    demo: 'https://kanboo.github.io/JavaScript30/03%20-%20CSS%20Variables/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/03%20-%20CSS%20Variables/index.html',
+    blog: 'https://kanboo.github.io/2018/02/15/JS30-03-CSS-Variables/',
+  },
+  {
+    no: 4,
+    title: 'Array Cardio Day 1',
+    desc: 'Array之filter、map、reduce、includes、sort、展開運算子、解構賦值 的運用',
+    demo: 'https://kanboo.github.io/JavaScript30/04%20-%20Array%20Cardio%20Day%201/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/04%20-%20Array%20Cardio%20Day%201/index.html',
+    blog: 'https://kanboo.github.io/2018/02/16/JS30-04-Array-Cardio-Day-1/',
+  },
+  {
+    no: 5,
+    title: 'Flex Panels Image Gallery',
+    desc: 'JS-Event 與 CSS-Flex 的搭配使用',
+    demo: 'https://kanboo.github.io/JavaScript30/05%20-%20Flex%20Panel%20Gallery/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/05%20-%20Flex%20Panel%20Gallery/index.html',
+    blog: 'https://kanboo.github.io/2018/02/26/JS30-05-Flex-Panel-Gallery/',
+  },
+  {
+    no: 6,
+    title: 'Ajax Type Ahead',
+    desc: 'fetch()、filter()、RegExp()正規表達式 的運用',
+    demo: 'https://kanboo.github.io/JavaScript30/06%20-%20Type%20Ahead/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/06%20-%20Type%20Ahead/index.html',
+    blog: 'https://kanboo.github.io/2018/02/26/JS30-06-Type-Ahead/',
+  },
+  {
+    no: 7,
+    title: 'Array Cardio Day 2',
+    desc: 'Array之some、every、find及展開運算子 的運用',
+    demo: 'https://kanboo.github.io/JavaScript30/07%20-%20Array%20Cardio%20Day%202/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/07%20-%20Array%20Cardio%20Day%202/index.html',
+    blog: 'https://kanboo.github.io/2018/02/26/JS30-07-Array-Cardio-Day-2/',
+  },
+  {
+    no: 8,
+    title: 'Fun with HTML5 Canvas',
+    desc: '使用滑鼠在 Canvas 上作畫',
+    demo: 'https://kanboo.github.io/JavaScript30/08%20-%20Fun%20with%20HTML5%20Canvas/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/08%20-%20Fun%20with%20HTML5%20Canvas/index.html',
+    blog: 'https://kanboo.github.io/2018/03/01/JS30-08-Fun-with-HTML5-Canvas/',
+  },
+  {
+    no: 9,
+    title: '14 Must Know Dev Tools Tricks',
+    desc: '運用 Chrome Debug 的工具',
+    demo: 'https://kanboo.github.io/JavaScript30/09%20-%20Dev%20Tools%20Domination/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/09%20-%20Dev%20Tools%20Domination/index.html',
+    blog: 'https://kanboo.github.io/2018/03/01/JS30-09-Dev-Tools-Domination/',
+  },
+  {
+    no: 10,
+    title: 'Hold Shift to Check Multiple Checkboxes',
+    desc: '搭配 Shift + 滑鼠 勾選多個Checkbox',
+    demo: 'https://kanboo.github.io/JavaScript30/10%20-%20Hold%20Shift%20and%20Check%20Checkboxes/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/10%20-%20Hold%20Shift%20and%20Check%20Checkboxes/index.html',
+    blog: 'https://kanboo.github.io/2018/03/03/JS30-10-Hold-Shift-and-Check-Checkboxes/',
+  },
+  {
+    no: 11,
+    title: 'Custom HTML5 Video Player',
+    desc: '利用Video屬性及Method來變更播放器的功能，播放/暫停、快進/快退、音量控制、倍數控制。',
+    demo: 'https://kanboo.github.io/JavaScript30/11%20-%20Custom%20Video%20Player/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/11%20-%20Custom%20Video%20Player/index.html',
+    blog: 'https://kanboo.github.io/2018/03/28/JS30-11-Custom-Video-Player/',
+  },
+  {
+    no: 12,
+    title: 'Key Sequence Detection',
+    desc: '輸入隱藏指令後，觸發特效的效果',
+    demo: 'https://kanboo.github.io/JavaScript30/12%20-%20Key%20Sequence%20Detection/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/12%20-%20Key%20Sequence%20Detection/index.html',
+    blog: 'https://kanboo.github.io/2018/03/31/JS30-12-Key-Sequence-Detection/',
+  },
+  {
+    no: 13,
+    title: 'Slide In on Scroll',
+    desc: '滾動捲軸，使圖片滑入顯示',
+    demo: 'https://kanboo.github.io/JavaScript30/13%20-%20Slide%20in%20on%20Scroll/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/13%20-%20Slide%20in%20on%20Scroll/index.html',
+    blog: 'https://kanboo.github.io/2018/04/09/JS30-13-Slide-in-on-Scroll/',
+  },
+  {
+    no: 14,
+    title: 'Object and Arrays - Reference VS Copy',
+    desc: '陣列與物件的傳址(reference)及複製(Copying)',
+    demo: 'https://kanboo.github.io/JavaScript30/14%20-%20JavaScript%20References%20VS%20Copying/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/14%20-%20JavaScript%20References%20VS%20Copying/index.html',
+    blog: 'https://kanboo.github.io/2018/04/18/JS30-14-JavaScript-References-VS-Copying/',
+  },
+  {
+    no: 15,
+    title: 'LocalStorage and Event Delegation',
+    desc: '使用 LocalStorage 做 todolist',
+    demo: 'https://kanboo.github.io/JavaScript30/15%20-%20LocalStorage/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/15%20-%20LocalStorage/index.html',
+    blog: 'https://kanboo.github.io/2018/04/18/JS30-15-LocalStorage/',
+  },
+  {
+    no: 16,
+    title: 'CSS Text Shadow Mouse Move Effect',
+    desc: '滑鼠移動時，讓文字的陰影跟著移動',
+    demo: 'https://kanboo.github.io/JavaScript30/16%20-%20Mouse%20Move%20Shadow/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/16%20-%20Mouse%20Move%20Shadow/index.html',
+    blog: 'https://kanboo.github.io/2018/04/18/JS30-16-Mouse-Move-Shadow/',
+  },
+  {
+    no: 17,
+    title: 'Sorting Band Names without articles',
+    desc: '將陣列中的資料，去除冠詞(Articles)後，再進行排序',
+    demo: 'https://kanboo.github.io/JavaScript30/17%20-%20Sort%20Without%20Articles/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/17%20-%20Sort%20Without%20Articles/index.html',
+    blog: 'https://kanboo.github.io/2018/04/18/JS30-17-Sort-Without-Articles/',
+  },
+  {
+    no: 18,
+    title: 'Tally String Times with Reduce',
+    desc: '利用 map() 與 reduce() 來計算播放清單的總秒數',
+    demo: 'https://kanboo.github.io/JavaScript30/18%20-%20Adding%20Up%20Times%20with%20Reduce/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/18%20-%20Adding%20Up%20Times%20with%20Reduce/index.html',
+    blog: 'https://kanboo.github.io/2018/04/18/JS30-18-Adding-Up-Times-with-Reduce/',
+  },
+  {
+    no: 19,
+    title: 'Unreal Webcam Fun',
+    desc: '利用視訊鏡頭顯示影片及拍照功能(+濾鏡效果)',
+    demo: 'https://kanboo.github.io/JavaScript30/19%20-%20Webcam%20Fun/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/19%20-%20Webcam%20Fun/index.html',
+    blog: 'https://kanboo.github.io/2018/04/20/JS30-19-Webcam-Fun/',
+  },
+  {
+    no: 20,
+    title: 'Native Speech Recognition',
+    desc: '利用麥克風說話，將語音轉成文字',
+    demo: 'https://kanboo.github.io/JavaScript30/20%20-%20Speech%20Detection/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/20%20-%20Speech%20Detection/index.html',
+    blog: 'https://kanboo.github.io/2018/04/21/JS30-20-Speech-Detection/',
+  },
+  {
+    no: 21,
+    title: 'Geolocation based Speedometer and Compass',
+    desc: '取得裝置的地理位置和速度',
+    demo: 'https://kanboo.github.io/JavaScript30/21%20-%20Geolocation/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/21%20-%20Geolocation/index.html',
+    blog: 'https://kanboo.github.io/2018/04/21/JS30-21-Geolocation/',
+  },
+  {
+    no: 22,
+    title: 'Follow Along Links',
+    desc: '滑鼠移動到特定位置產生highlight效果',
+    demo: 'https://kanboo.github.io/JavaScript30/22%20-%20Follow%20Along%20Link%20Highlighter/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/22%20-%20Follow%20Along%20Link%20Highlighter/index.html',
+    blog: 'https://kanboo.github.io/2018/04/21/JS30-22-Follow-Along-Link-Highlighter/',
+  },
+  {
+    no: 23,
+    title: 'Speech Synthesis',
+    desc: '將文字轉語音(可變更速率及音調)',
+    demo: 'https://kanboo.github.io/JavaScript30/23%20-%20Speech%20Synthesis/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/23%20-%20Speech%20Synthesis/index.html',
+    blog: 'https://kanboo.github.io/2018/04/21/JS30-23-Speech-Synthesis/',
+  },
+  {
+    no: 24,
+    title: 'Sticky Nav',
+    desc: '利用一些小技巧，處理navbar的動畫效果',
+    demo: 'https://kanboo.github.io/JavaScript30/24%20-%20Sticky%20Nav/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/24%20-%20Sticky%20Nav/index.html',
+    blog: 'https://kanboo.github.io/2018/04/21/JS30-24-Sticky-Nav/',
+  },
+  {
+    no: 25,
+    title: 'Event Capture, Propagation, Bubbling and Once',
+    desc: '了解 addEventListener 的運作',
+    demo: 'https://kanboo.github.io/JavaScript30/25%20-%20Event%20Capture,%20Propagation,%20Bubbling%20and%20Once/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/25%20-%20Event%20Capture,%20Propagation,%20Bubbling%20and%20Once/index.html',
+    blog: 'https://kanboo.github.io/2018/04/23/JS30-25-Event-Capture-Propagation-Bubbling-and-Once/',
+  },
+  {
+    no: 26,
+    title: 'Stripe Follow Along Dropdown',
+    desc: 'navbar次導覽列的動態效果',
+    demo: 'https://kanboo.github.io/JavaScript30/26%20-%20Stripe%20Follow%20Along%20Nav/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/26%20-%20Stripe%20Follow%20Along%20Nav/index.html',
+    blog: 'https://kanboo.github.io/2018/04/23/JS30-26-Stripe-Follow-Along-Nav/',
+  },
+  {
+    no: 27,
+    title: 'Click and Drag to Scroll',
+    desc: '可用滑鼠點擊拖曳水平的畫面',
+    demo: 'https://kanboo.github.io/JavaScript30/27%20-%20Click%20and%20Drag/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/27%20-%20Click%20and%20Drag/index.html',
+    blog: 'https://kanboo.github.io/2018/04/24/JS30-27-Click-and-Drag/',
+  },
+  {
+    no: 28,
+    title: 'Video Speed Controller UI',
+    desc: '用滑鼠控制影片播放速率',
+    demo: 'https://kanboo.github.io/JavaScript30/28%20-%20Video%20Speed%20Controller/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/28%20-%20Video%20Speed%20Controller/index.html',
+    blog: 'https://kanboo.github.io/2018/04/26/JS30-28-Video-Speed-Controller/',
+  },
+  {
+    no: 29,
+    title: 'Countdown Clock',
+    desc: '完成可自定義時間的倒數計時器',
+    demo: 'https://kanboo.github.io/JavaScript30/29%20-%20Countdown%20Timer/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/29%20-%20Countdown%20Timer/index.html',
+    blog: 'https://kanboo.github.io/2018/04/26/JS30-29-Countdown-Timer/',
+  },
+  {
+    no: 30,
+    title: 'Whack A Mole Game',
+    desc: '打地鼠遊戲',
+    demo: 'https://kanboo.github.io/JavaScript30/30%20-%20Whack%20A%20Mole/',
+    github: 'https://github.com/kanboo/JavaScript30/blob/master/30%20-%20Whack%20A%20Mole/index.html',
+    blog: 'https://kanboo.github.io/2018/04/27/JS30-30-Whack-A-Mole/',
+  },
+
+]
+
+
+const loadDatas = () => {
+  const courseList = document.querySelector('.courseList');
+  const courseHtml = datas.map(item => {
+    return `
+    <div class="courseList__item">
+        <div class="course__header">
+          <div class="course__no">Day ${item.no}</div>
+          <img src="https://s3.amazonaws.com/js30-cdn/small${item.no -1 }.jpg" alt="${item.no}" class="">
+        </div>
+
+        <div class="course__body">
+            <h2 class="course__title">#${item.no} - ${item.title}</h2>
+            <div class="course__desc">${item.desc}</div>
+        </div>
+
+        <div class="course__footer">
+          <a class="course__btn" href="${item.demo}" target="_blank">DEMO</a>
+          <a class="course__btn" href="${item.github}" target="_blank">GitHub</a>
+          <a class="course__btn" href="${item.blog}" target="_blank">Blog</a>
+        </div>
+      </div>
+    `
+  }).join('');
+
+  // console.log(courseHtml);
+
+  courseList.innerHTML = courseHtml;
+}
+
+
+(function () {
+  try {
+    new Function('(aa = 0) => aa');
+    loadDatas();
+  } catch (err) {
+    document.querySelector('.courseList').innerHTML =
+      '<h2>此網站有使用ES6的語法，若有你有看到此訊息的話，<br>請升級你的瀏覽器或使用最新的Chrome吧!</h2>';
+  }
+})();
